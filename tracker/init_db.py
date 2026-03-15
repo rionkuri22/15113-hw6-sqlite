49d8749ada2f93d28ff6f9027614984ec49a254b
rionkuri22 <rion.kurihara622@gmail.com>
2026-03-15 02:42:17 -0400

    initial commit

---
+++ tracker/init_db.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+# Connect to (or create) a database file
+con = sqlite3.connect('my_tracker.db')
+cur = con.cursor()
+
+# Create your table (if it doesn't already exist)
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS books (
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        author TEXT,
+        rating INTEGER,
+        date_finished TEXT,
+        notes TEXT
+    )
+''')
+con.commit()
